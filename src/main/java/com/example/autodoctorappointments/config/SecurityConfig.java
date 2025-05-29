ceddfb6058460b78ee2141db058c1340e244eb6d
ishenaly007 <ishennarkozuev07@gmail.com>
2025-05-29 15:31:04 +0600

    ывсампи

--- src/main/java/com/example/autodoctorappointments/config/SecurityConfig.java
+++ src/main/java/com/example/autodoctorappointments/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://ab1t.top"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

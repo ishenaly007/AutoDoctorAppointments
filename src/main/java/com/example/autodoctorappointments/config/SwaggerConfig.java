8e7bb452ec8a30d9ac9500e4847d5bef77bb2767
ishenaly007 <ishennarkozuev07@gmail.com>
2025-06-11 23:38:49 +0600

    change config

--- src/main/java/com/example/autodoctorappointments/config/SwaggerConfig.java
+++ src/main/java/com/example/autodoctorappointments/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -31,6 +34,11 @@ public class SwaggerConfig {
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(
+                        new Server()
+                                .url("https://ab1t.top")
+                                .description("Production server")
+                ));
     }
 }
